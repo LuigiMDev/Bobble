ae42e39e353cd558b7819b7f58f6f93c143e90cb
Luigi <luigicumaru@gmail.com>
2024-09-25 16:58:49 -0300

    Mensagem de Erro

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,22 +3,33 @@ import React, { useState } from 'react'
 
 const Search = () => {
     const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    const [error, setError] = useState("");
+
     const API_KEY = "8b629976f774f1a3d700699d3d74daf640d31c77a6b7fa07a5b3b1199e29e304";
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const res = axios.get("https://serpapi.com/search.json", {
-            params: {
-                q: query,
-                engine: "google",
-                google_domain: "google.com.br",
-                api_key: API_KEY,
-                hl: "pt-br",
-                gl: "br",
-                num: 10,
-            }
-        })
+        try {
+            const res = axios.get("https://serpapi.com/search.json", {
+                params: {
+                    q: query,
+                    engine: "google",
+                    google_domain: "google.com.br",
+                    api_key: API_KEY,
+                    hl: "pt-br",
+                    gl: "br",
+                    num: 10,
+                }
+            })
+            const data = await res.json();
+            setResults(data);
+        } catch (err) {
+            console.error(err);
+            setError("Ocorreu um erro ao fazer a busca.");
+        }
+
     }
 
 
